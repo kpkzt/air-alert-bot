0d64a1a864d6b64bf12cd2054451400e6447ad0d
kpkzt <karaman@kpkzt.kyiv.ua>
2025-04-06 16:17:48 +0300

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ last_alert_status = None
 
 # Дані для SendPulse
 BOT_ID = "6571c6acaa0e5f3a19061706"
-TOKEN = "Bearer ВСТАВ_ТУТ_СВІЙ_ТОКЕН"  # Не забудь вставити свій токен сюди
+TOKEN = "Bearer 6905718299:AAEamF5A4n1ff80pVZuF2J5GlgEYqTz9l-M" # Не забудь вставити свій токен сюди
 
 def check_air_alert():
     global last_alert_status
